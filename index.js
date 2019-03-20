1669290aada2e9cb7f1b54f7187d988478631cd9
Daniel Killoran <dankilloran@gmail.com>
2019-03-20 23:55:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"]
 var destructivelyAppendKitten = function() {
   return kittens.push("Ralph")
 }
+
+var destructivelyAppendKitten = function() {
+  return kittens.unshift("Ralph")
+}
