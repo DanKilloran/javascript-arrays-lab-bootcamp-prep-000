4df67b886be9e355776a0907d6f585a2b95f9265
Daniel Killoran <dankilloran@gmail.com>
2019-03-21 01:20:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,3 +29,7 @@ var prependKitten = function() {
 var removeLastKitten = function() {
   return kittens.slice(0, kittens.length - 1)
 }
+
+var removeLastKitten = function() {
+  return kittens.slice(1)
+}
